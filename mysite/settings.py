f93e850c0cd543f98d1915f90341a13b1012b96c
SirapopKunjiak <sirapop.ku@ku.th>
2020-10-31 20:36:52 +0700

    travis fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '14_#ue^+m0a%38$$3l*&_q%p5m3%(%2-!d@b#*0pkr&eex@6=n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
